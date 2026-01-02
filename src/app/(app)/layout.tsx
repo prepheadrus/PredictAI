a0f378035c3fbbc58c790b737fa6c0ac0776f5d9
Soligann <prepheadrus@gmail.com>
2026-01-02 06:51:25 +0000

    sorun sayfada soldaki menü ile sağdaki verilerin gösterildiği sayfanın b

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,10 +4,10 @@ import { SidebarNav } from "@/components/layout/sidebar-nav";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="flex min-h-screen w-full bg-muted/40">
         <SidebarNav />
-        <div className="flex flex-col sm:pl-14">
-          <main className="flex-1 overflow-y-auto pt-8">
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 w-full">
+          <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
             {children}
           </main>
         </div>
