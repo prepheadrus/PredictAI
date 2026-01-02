bacbfc329be9cb7df9b032ccc7f463e7ebba1d1e
Soligann <prepheadrus@gmail.com>
2026-01-02 06:56:40 +0000

    sayfa yapısını gene iki sütünlu olmaktan çıkarıp tek sütünlü bir yapıya

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,8 +6,8 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <div className="flex min-h-screen w-full bg-muted/40">
         <SidebarNav />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 w-full">
-          <main className="flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <div className="flex flex-col sm:pl-14 w-full">
+          <main className="flex-1 items-start gap-4 p-4 sm:px-6 md:gap-8">
             {children}
           </main>
         </div>
