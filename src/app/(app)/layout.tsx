4db7a4827932467bc6189f62fe4aec54e98c6b73
Soligann <prepheadrus@gmail.com>
2026-01-02 06:41:06 +0000

    o boşluk tüm sayfalarda varç,. sa,,yfaların üstünde şu anda bir boşluğa

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,9 +7,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <SidebarNav />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <div className="flex flex-col sm:pl-14">
           <Header />
-          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+          <main className="flex-1">
             {children}
           </main>
         </div>
