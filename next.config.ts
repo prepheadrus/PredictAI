0a4b86b7cddc026f7f49c7e6bd486545fb9db8aa
Soligann <prepheadrus@gmail.com>
2026-01-04 19:13:07 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://crests.

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'crests.football-data.org',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
